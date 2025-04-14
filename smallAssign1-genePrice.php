710e20692d364c521aed0c0b23fa088d4bb50944
Rei_Hagiwara <rei.hagiwara@tokyo.supersoftware.co.jp>
2025-04-14 12:55:22 +0900

    少し変更

--- smallAssign1-genePrice.php
+++ smallAssign1-genePrice.php
@@ -1,9 +1,9 @@
 <?php
         //価格設定に基づいて、15個分の価格を出すクラス
         class GenePrice{
-            function generatePrices( $count, $min, $max ){
+            function genePrice( $count, $min, $max ){
                 $prices = [];
-                for( $i = 1; $i <= 15; $i++ ){
+                for( $i = 1; $i <= $count; $i++ ){
                     $prices[$i] = random_int( $min, $max );
                 }
                 return $prices;
